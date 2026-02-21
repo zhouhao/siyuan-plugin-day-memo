4cf012c5315413b56e9719179a284b907a4aa720
SaltyEgg <hzhou.me@saltyee.com>
2026-02-20 23:25:39 -0800

    update deployment target and build output path

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-TARGET="$HOME/SiYuan/data/plugins/siyuan-plugin-kanban"
-pnpm build && cp -f index.js index.css "$TARGET/"
+TARGET="$HOME/SiYuan/data/plugins/siyuan-plugin-day-memo"
+pnpm build && cp -f dist/* "$TARGET/"
 echo "Deployed to $TARGET"
